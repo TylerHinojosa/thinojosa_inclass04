1894e2893d437a71066d2d1450144bc47fb2f5ae
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-05-08 19:39:34 -0500

    Initial commit

---
+++ thinojosa_inclass04/thinojosa_inclass04/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thinojosa_inclass04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "Hello, World!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = txtInput.Text;
+            txtInput.Text = "";
+        }
+    }
+}
